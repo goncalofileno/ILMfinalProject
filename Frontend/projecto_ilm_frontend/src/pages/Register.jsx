41ce711e3835718e4eb10b63ec4e67fc34a10621
Bernardocamara96 <camarabernardo96@gmail.com>
2024-05-22 11:27:57 +0100

    update frontend-login-bootstrap

--- Frontend/projecto_ilm_frontend/src/pages/Register.jsx
+++ Frontend/projecto_ilm_frontend/src/pages/Register.jsx
@@ -1,15 +1,28 @@
+import "./App.css";
+import { useNavigate } from "react-router-dom";
 import RegisterForm from "../forms/RegisterForm";
 import LoginHeader from "../headers/LoginHeader";
-import "./App.css";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import LoginProjectsCards from "../cards/LoginProjectsCards";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
 
-export default function Register() {
+function Register() {
+   const navigate = useNavigate();
    const contentRef = useRef(null);
+   const isComputer = useMediaQuery({ minWidth: 1200 });
+   const isTablet = useMediaQuery({ minWidth: 992, maxWidth: 1200 });
+   const isSmallTablet = useMediaQuery({ minWidth: 768, maxWidth: 992 });
+   const isPhone = useMediaQuery({ maxWidth: 768 });
+   const headerHeight = 60;
 
    const scrollToContent = () => {
-      contentRef.current.scrollIntoView({ behavior: "smooth" });
+      contentRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+      if (!isComputer) {
+         setTimeout(() => {
+            window.scrollBy(0, -headerHeight);
+         }, 500);
+      }
    };
 
    const scrollToLogin = () => {
@@ -18,116 +31,150 @@ export default function Register() {
          behavior: "smooth", // Smooth scroll effect
       });
    };
+
    return (
       <>
          <LoginHeader />
-         <div className="background-img-container">
-            <div className="background-img-app"></div>
-         </div>
-         <div>
-            <div className="container-fluid ilm-page container-login-page ">
-               <Row>
-                  <Col sm={4}>
-                     <h1 className="ilm-general-title" id="slogan-app">
-                        Where Ideas
-                     </h1>
-                  </Col>
-                  <Col sm={4} id="col-description">
-                     <h1 className="ilm-general-title">
-                        <span>Take Flight</span>
-                     </h1>
-                     <div className="ilm-description-login-register">
-                        <div>
-                           Welcome to our Innovation Lab Management app, where creativity meets collaboration and ideas
-                           turn into reality. With our app, you can manage and track your projects seamlessly, from
-                           brainstorming to implementation, ensuring every idea has the potential to flourish. Join us
-                           in driving progress and inspiring change, one innovative project at a time.
-                        </div>
-                        <button className="submit-button" id="projects-button" onClick={scrollToContent}>
-                           Some of our Projects
-                        </button>
-                     </div>
-                  </Col>
-                  <Col sm={4}>
-                     <RegisterForm />
-                  </Col>
-               </Row>
+         <div className="page-content">
+            <div className={isComputer ? "ilm-page" : isTablet ? "ilm-pageb" : "ilm-page-noheight"}>
+               <Container fluid className="outer-container">
+                  <Row className="outer-row">
+                     <Col xl={8} lg={6} sm={12}>
+                        <Row>
+                           <Col xl={6} lg={12} md={6} sm={12}>
+                              <h1 className="app-slogan app-slogan-1" style={{ textAlign: isSmallTablet && "end" }}>
+                                 Where Ideas
+                              </h1>
+                           </Col>
+                           <Col xl={6} lg={12} md={6} sm={12}>
+                              <h1
+                                 className="app-slogan app-slogan-2"
+                                 style={{ textAlign: (isTablet || isPhone) && "center" }}
+                              >
+                                 Take Flight
+                              </h1>
+                           </Col>
+                        </Row>
+                        <Row>
+                           {isComputer && <Col lg={6}></Col>}
+                           {!isPhone && (
+                              <Col>
+                                 {" "}
+                                 <div className="ilm-description">
+                                    Welcome to our Innovation Lab Management app, where creativity meets collaboration
+                                    and ideas turn into reality. With our app, you can manage and track your projects
+                                    seamlessly, from brainstorming to implementation, ensuring every idea has the
+                                    potential to flourish. Join us in driving progress and inspiring change, one
+                                    innovative project at a time.
+                                 </div>
+                                 <div className="div-project-button">
+                                    <button
+                                       className="submit-button"
+                                       id="projects-button"
+                                       onClick={scrollToContent}
+                                       style={{ marginBottom: !isComputer && !isTablet && "100px" }}
+                                    >
+                                       Some of our Projects
+                                    </button>
+                                 </div>
+                              </Col>
+                           )}
+                        </Row>
+                     </Col>
+                     <Col xl={4} lg={6} className="align-items-center" style={{ marginTop: isPhone && "50px" }}>
+                        <RegisterForm />
+                     </Col>
+                     {isPhone && (
+                        <Col>
+                           <div className="div-project-button">
+                              <button className="submit-button" id="projects-button" onClick={scrollToContent}>
+                                 Some of our Projects
+                              </button>
+                           </div>
+                        </Col>
+                     )}
+                  </Row>
+               </Container>
             </div>
-            <div className=" container-fluid ilm-projects-login-page ilm-page2" ref={contentRef}>
-               <Row>
-                  <h1 className="ilm-general-subTitle" id="projects-title">
-                     Projects
-                  </h1>
-               </Row>
-               <Row className="row-project-cards-login">
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color1"
-                        title="Aviation"
-                        description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
-                        scrollToRef={scrollToLogin}
-                     />
-                  </Col>
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color1"
-                        title="Defence & Security"
-                        description="Peacekeeping relies on real-time information. We offer an arsenal of data system, technology and coordination support to make sure your defence projects go without a hitch."
-                     />
-                  </Col>
-
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color2"
-                        title="Defence & Security"
-                        description="Peacekeeping relies on real-time information. We offer an arsenal of data system, technology and coordination support to make sure your defence projects go without a hitch."
-                     />
-                  </Col>
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color2"
-                        title="Defence & Security"
-                        description="Peacekeeping relies on real-time information. We offer an arsenal of data system, technology and coordination support to make sure your defence projects go without a hitch."
-                     />
-                  </Col>
-               </Row>
-               <Row className="row-project-cards-login">
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color3"
-                        title="Defence & Security"
-                        description="Peacekeeping relies on real-time information. We offer an arsenal of data system, technology and coordination support to make sure your defence projects go without a hitch."
-                     />
-                  </Col>
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color3"
-                        title="Defence & Security"
-                        description="Peacekeeping relies on real-time information. We offer an arsenal of data system, technology and coordination support to make sure your defence projects go without a hitch."
-                     />
-                  </Col>
-                  <Col sm={3}>
-                     <LoginProjectsCards
-                        cardBkgColor="card-bkg-color4"
-                        title="Defence & Security"
-                        description="Peacekeeping relies on real-time information. We offer an arsenal of data system, technology and coordination support to make sure your defence projects go without a hitch."
-                     />
-                  </Col>
-
-                  <Col sm={3}>
-                     <div id="see-more-projects-div">
-                        <button
-                           className="submit-button btn-with-up-arrow"
-                           style={{ width: "40.5vh" }}
-                           onClick={scrollToLogin}
-                        >
-                           Login to see more projects <i class="fas fa-arrow-up margin-left-arrow"></i>
-                        </button>
-                     </div>
-                  </Col>
-               </Row>
+            <div className="ilm-page2" ref={contentRef}>
+               <Container className="outer-container">
+                  <Row>
+                     <h1 className="ilm-general-subTitle">Projects</h1>
+                  </Row>
+                  <Row className="row-margin-top-20 last-row-padd-bott">
+                     <Col lg={3} md={6} className={!isComputer && !isTablet && "col-margin-bottom-16"}>
+                        <div className="flex-col-gap-16">
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color1"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color1"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                        </div>
+                     </Col>
+                     <Col lg={3} md={6} className={!isComputer && !isTablet && "col-margin-bottom-16"}>
+                        <div className="flex-col-gap-16">
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color4"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color4"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                        </div>
+                     </Col>
+                     <Col lg={3} md={6} className={!isComputer && !isTablet && "col-margin-bottom-16"}>
+                        <div className="flex-col-gap-16">
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color2"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color2"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                        </div>
+                     </Col>
+                     <Col lg={3} md={6}>
+                        <div className="flex-col-gap-16" id="last-column-projects">
+                           <LoginProjectsCards
+                              cardBkgColor="card-bkg-color3"
+                              title="Aviation"
+                              description="From software architectures to system testing, we’ll look after your project from take-off to landing, combining our broad expertise with an in-depth knowledge of the rigorous safety standards in aviation."
+                              scrollToRef={scrollToLogin}
+                           />
+                           <div className="see-more-projects-button-div" style={{ height: isPhone && "100px" }}>
+                              <div
+                                 className="submit-button btn-with-up-arrow"
+                                 onClick={scrollToLogin}
+                                 id="see-more-projects-button"
+                              >
+                                 Login to see more projects <i class="fas fa-arrow-up margin-left-arrow"></i>
+                              </div>
+                           </div>
+                        </div>
+                     </Col>
+                  </Row>
+               </Container>
             </div>
          </div>
       </>
    );
 }
+
+export default Register;
