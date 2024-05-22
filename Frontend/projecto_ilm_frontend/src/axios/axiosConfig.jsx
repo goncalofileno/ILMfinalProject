83babe283901003a6c3b5aa873e0d46c1020d67e
Bernardocamara96 <camarabernardo96@gmail.com>
2024-05-22 17:12:42 +0100

    update services

---
+++ Frontend/projecto_ilm_frontend/src/axios/axiosConfig.jsx
@@ -0,0 +1,16 @@
+/*import axios from "axios";
+import useBaseUrlStore from "../stores/useBaseUrlStore";
+const useAxios = () => {
+   const baseUrl = useBaseUrlStore((state) => state.baseUrl);
+
+   const instance = axios.create({
+      baseURL: baseUrl,
+      headers: {
+         "Content-Type": "application/json",
+      },
+   });
+
+   return instance;
+};
+
+export default useAxios;*/
