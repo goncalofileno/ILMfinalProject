d4ac6d6fe9886bde358afa1a73f95404710b8026
Bernardocamara96 <camarabernardo96@gmail.com>
2024-05-27 14:30:06 +0100

    commit

--- Backend/projecto_ILM_backend/src/main/java/com/ilm/projecto_ilm_backend/utilities/imgsPath.java
+++
@@ -1,9 +0,0 @@
-package com.ilm.projecto_ilm_backend.utilities;
-
-
-
-public class imgsPath {
-
-    public static final String IMAGES_PATH = "/Users/goncalofileno/Servers/wildfly-31.0.1.Final/photos/";
-
-}
